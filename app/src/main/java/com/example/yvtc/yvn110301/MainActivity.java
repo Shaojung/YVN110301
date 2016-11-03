b5979b6dc6356ab709112fce5b43cfc3c7c1c7f3
shaojung <shaojung@gmail.com>
2016-11-03 18:54:22 +0800

    Add Options Menu

--- app/src/main/java/com/example/yvtc/yvn110301/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvn110301/MainActivity.java
@@ -16,14 +16,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add("設定");
-        menu.add("關於我們");
+        menu.add(0, 0, 0, "設定");
+        menu.add(0, 1, 1, "關於我們");
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getTitle().toString().equals("關於我們"))
+        if (item.getItemId() == 1)
         {
             Toast.makeText(MainActivity.this, "這是關於我們", Toast.LENGTH_SHORT).show();
         }
